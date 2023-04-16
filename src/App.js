09d849c5d312ac1bd1c87e8271f987e1fc92101b
katechhka <kalininak03@gmail.com>
2023-04-16 16:43:54 +0300

    week31

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import React, {useState} from "react";
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
@@ -12,14 +12,20 @@ import {
 } from "react-router-dom";
 
 function App() {
+
+  const [learnWords, setLearnWords]= useState(0);
+  const handleLearned =() =>{
+    setLearnWords(learnWords+1)
+  }
   return (
     <Router>
     <div className="App">
       <Header/>
       <Routes>
           <Route path="/" element={<CreateTable />} />
-          <Route path="/game" element={<Card />} />
+          <Route path="/game" element={<Card onLearned={handleLearned}/>} />
         </Routes>
+        <div>Количество выученных слов: {learnWords}</div>
       <Footer/>
     </div>
     </Router>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,42 +1,57 @@
-import React, {useState} from "react";
+import React, {useState, useRef, useEffect} from "react";
 import "./Card.css";
 import words from '../../utils/words';
 import arrow from '../../images/left-arrow.png';
-function Card() {
-  const [isSelected, setisSelected] = useState(false);
 
+function Card(props) {
+  const [isSelected, setisSelected] = useState(false);
+  const [isView, setIsView]=useState(false);
   const [index, setId] = useState(0);
 
   let cardItem=words[index];
 
   const handleChange=()=>{
     setisSelected(!isSelected);
+    if(!isView){
+      setIsView(true);
+    props.onLearned();
+    }
   }
   const handlePrev=()=>{
+    if(isSelected){
+      setisSelected(!isSelected)};
     if(index-1<0){
       setId(words.length-1);
     }
     else{
       setId(index-1);
     }
-    
+    setIsView(false);
   }
   const handleNext=()=>{
+    if(isSelected){
+    setisSelected(!isSelected)}
     if(index+1>=words.length){
       setId(0);
+      
     }
     else{
       setId(index+1);
     }
+    setIsView(false);
   }
 
+  const ref= useRef();
+  useEffect(()=>{
+    ref.current.focus();
+  }, []);
   
   return (
     <div className="card__container">
     <div className="card" {...index}>
        <h2 className="card__english">{cardItem.english}</h2>
        <div className="card__transcription">{cardItem.transcription}</div>
-       <button className={"card__button " +(isSelected? "card__button_hidden":"")} onClick={handleChange}>Проверить</button>
+       <button className={"card__button " +(isSelected? "card__button_hidden":"")} onClick={handleChange} ref={ref}>Проверить</button>
        <div className={"card__translate_hidden "+(isSelected? "card__translate":"")} onClick={handleChange}>{cardItem.russian}</div>
     </div>
     <div className="card__buttons">
